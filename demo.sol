6c7c4e95badf137619f95143b9154f9d0fdccd9f
abhay <abhaytxanax@gmail.com>
2022-12-04 22:27:38 +0530

    SampleProgram

--- demo.sol
+++ demo.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.10;
 
 contract demo {
-    string public s = "Hello World";
+    // string public s = "Hello World";
+    int256 number;
+
+    constructor() {
+        number = 5;
+    }
+
+    function getter() public view returns (int256) {
+        return number;
+    }
+
+    function increment() public {
+        number = number + 1;
+    }
 }
